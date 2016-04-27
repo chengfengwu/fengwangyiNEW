13baa3ddf117281bf7245cbab4641e75201dbbae
Your Name <you@example.com>
2016-04-27 14:10:15 +0800

    新建项目

---
+++ wangyinew/wangyinew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wangyinew
+//
+//  Created by admin on 16-4-27.
+//  Copyright (c) 2016年 Wind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wangyinew/wangyinew/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wangyinew
+//
+//  Created by admin on 16-4-27.
+//  Copyright (c) 2016年 Wind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
